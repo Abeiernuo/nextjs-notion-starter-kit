def41414377b7115917f0539b425f8b1eae55864
Abeiernuo <103920464+Abeiernuo@users.noreply.github.com>
2024-08-20 11:50:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'eb23b3c41c9844eaabc051cccdc82eb6',
+  rootNotionPageId: '16f8bf7f29d845abb2ff017722a3b8f8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '小玉儿のblog',
-  domain: 'https://educated-yew-904.notion.site/eb23b3c41c9844eaabc051cccdc82eb6?pvs=4',
+  domain: 'https://educated-yew-904.notion.site/CloudFlare-Workers-16f8bf7f29d845abb2ff017722a3b8f8?pvs=4',
   author: '小玉儿挖星星',
 
   // open graph metadata (optional)
