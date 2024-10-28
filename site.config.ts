4de098220845bad1a9f2c8c0008e656fd7b871a2
Abeiernuo <103920464+Abeiernuo@users.noreply.github.com>
2024-10-28 14:01:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f3a8636d20d7411785d158cf8f6fffbb',
+  rootNotionPageId: '12c68ca065868058854ae9e17e4c557f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '小玉儿のblog',
-  domain: 'https://educated-yew-904.notion.site/blog-f3a8636d20d7411785d158cf8f6fffbb?pvs=4',
-  author: '小玉儿挖星星',
+  name: '含羞草',
+  domain: 'https://puddle-network-046.notion.site/12c68ca065868058854ae9e17e4c557f?pvs=4',
+  author: '含羞草',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: '',
-  github: 'Abeiernuo',
+  github: '',
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
